e53fc1b2d46bc4a609684e93ef7d9f07c779b8e4
cxsquared <cxsquared@gmail.com>
2024-01-27 11:48:20 -0500

    Working on score

--- TestImmediate.gd
+++ TestImmediate.gd
@@ -1,10 +1,11 @@
 extends MeshInstance3D
 
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Begin draw.
-	mesh.surface_begin(Mesh.PRIMITIVE_TRIANGLES)
+	mesh.surface_begin(Mesh.PRIMITIVE_TRIANGLE_STRIP)
 
 	# Prepare attributes for add_vertex.
 	mesh.surface_set_normal(Vector3(0, 0, 1))
@@ -22,63 +23,51 @@ func _ready():
 	
 	# Prepare attributes for add_vertex.
 	mesh.surface_set_normal(Vector3(0, 0, 1))
-	mesh.surface_set_uv(Vector2(0, 0))
-	# Call last for each vertex, adds the above attributes.
-	mesh.surface_add_vertex(Vector3(-1, -1, 0))
-	
-	mesh.surface_set_normal(Vector3(0, 0, 1))
-	mesh.surface_set_uv(Vector2(1, 1))
-	mesh.surface_add_vertex(Vector3(1, 1, 0))
-
-	mesh.surface_set_normal(Vector3(0, 0, 1))
 	mesh.surface_set_uv(Vector2(1, 0))
+	# Call last for each vertex, adds the above attributes.
 	mesh.surface_add_vertex(Vector3(1, -1, 0))
 
 	# End drawing.
 	mesh.surface_end()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	extends MeshInstance3D
-
-func _process(delta):
-
-	# Clean up before drawing.
-	mesh.clear_surfaces()
-
-	# Begin draw.
-	mesh.surface_begin(Mesh.PRIMITIVE_TRIANGLES)
-
-	# Draw mesh.
-		# Prepare attributes for add_vertex.
-	mesh.surface_set_normal(Vector3(0, 0, 1))
-	mesh.surface_set_uv(Vector2(0, 0))
-	# Call last for each vertex, adds the above attributes.
-	mesh.surface_add_vertex(Vector3(-1, -1, 0))
-
-	mesh.surface_set_normal(Vector3(0, 0, 1))
-	mesh.surface_set_uv(Vector2(0, 1))
-	mesh.surface_add_vertex(Vector3(-1, 1, 0))
-
-	mesh.surface_set_normal(Vector3(0, 0, 1))
-	mesh.surface_set_uv(Vector2(1, 1))
-	mesh.surface_add_vertex(Vector3(1, 1, 0))
-	
-	# Prepare attributes for add_vertex.
-	mesh.surface_set_normal(Vector3(0, 0, 1))
-	mesh.surface_set_uv(Vector2(0, 0))
-	# Call last for each vertex, adds the above attributes.
-	mesh.surface_add_vertex(Vector3(-1, -1, 0))
-	
-	mesh.surface_set_normal(Vector3(0, 0, 1))
-	mesh.surface_set_uv(Vector2(1, 1))
-	mesh.surface_add_vertex(Vector3(1 * math.randf(), 1 * math.randf(), 0))
-
-	mesh.surface_set_normal(Vector3(0, 0, 1))
-	mesh.surface_set_uv(Vector2(1, 0))
-	mesh.surface_add_vertex(Vector3(1 * math.randf(), -1 * math.randf(), 0))
-
-	# End drawing.
-	mesh.surface_end()
-	pass
+#func _process(delta):
+#
+#	# Clean up before drawing.
+#	mesh.clear_surfaces()
+#
+#	# Begin draw.
+#	mesh.surface_begin(Mesh.PRIMITIVE_TRIANGLES)
+#
+#	# Draw mesh.
+#		# Prepare attributes for add_vertex.
+#	mesh.surface_set_normal(Vector3(0, 0, 1))
+#	mesh.surface_set_uv(Vector2(0, 0))
+#	# Call last for each vertex, adds the above attributes.
+#	mesh.surface_add_vertex(Vector3(-1, -1, 0))
+#
+#	mesh.surface_set_normal(Vector3(0, 0, 1))
+#	mesh.surface_set_uv(Vector2(0, 1))
+#	mesh.surface_add_vertex(Vector3(-1, 1, 0))
+#
+#	mesh.surface_set_normal(Vector3(0, 0, 1))
+#	mesh.surface_set_uv(Vector2(1, 1))
+#	mesh.surface_add_vertex(Vector3(1, 1, 0))
+#
+#	# Prepare attributes for add_vertex.
+#	mesh.surface_set_normal(Vector3(0, 0, 1))
+#	mesh.surface_set_uv(Vector2(0, 0))
+#	# Call last for each vertex, adds the above attributes.
+#	mesh.surface_add_vertex(Vector3(-1 *  randf_range(.5, 1.), -1 *  randf_range(.5, 1.), 0))
+#
+#	mesh.surface_set_normal(Vector3(0, 0, 1))
+#	mesh.surface_set_uv(Vector2(1, 1))
+#	mesh.surface_add_vertex(Vector3(1 * randf_range(.5, 1.), 1 *  randf_range(.5, 1.), 0))
+#
+#	mesh.surface_set_normal(Vector3(0, 0, 1))
+#	mesh.surface_set_uv(Vector2(1, 0))
+#	mesh.surface_add_vertex(Vector3(1 *  randf_range(.5, 1.), -1 *  randf_range(.5, 1.), 0))
+#
+#	# End drawing.
+#	mesh.surface_end()
+#	pass
--- alien.gd
+++ alien.gd
@@ -10,10 +10,42 @@ const EXPORT_PATH: String = "res://export_mesh.tres"
 
 var vertex_grabbers: Dictionary = {}
 var mdt: MeshDataTool = null
+var sad_mdt: MeshDataTool = null
+var amused_mdt: MeshDataTool = null
+var angry_mdt: MeshDataTool = null
+
+@export var max_distance_score:int = 1000
+@export var min_distance_score:int = 100
+
+var sad_score:Range
+var amused_score:Range
+var angry_score:Range
+
+@export var ui:CanvasLayer
 
 @onready var VertexGrabberScene = preload("res://vertex_grabber.tscn")
 
 func _ready() -> void:
+	
+	#Init emotions
+	sad_mdt = MeshDataTool.new()
+	var sad_surface_tool = SurfaceTool.new()
+	sad_surface_tool.create_from($SadReferenceMesh.mesh, 0)
+	var sad_array_mesh = sad_surface_tool.commit()
+	sad_mdt.create_from_surface(sad_array_mesh, 0)
+	
+	amused_mdt = MeshDataTool.new()
+	var amused_surface_tool = SurfaceTool.new()
+	amused_surface_tool.create_from($AmusedReferenceMesh.mesh, 0)
+	var amused_array_mesh = amused_surface_tool.commit()
+	amused_mdt.create_from_surface(amused_array_mesh, 0)
+	
+	angry_mdt = MeshDataTool.new()
+	var angry_surface_tool = SurfaceTool.new()
+	angry_surface_tool.create_from($AngryReferenceMesh.mesh, 0)
+	var angry_array_mesh = angry_surface_tool.commit()
+	angry_mdt.create_from_surface(angry_array_mesh, 0)
+	
 	# Init the MeshDataTool with the reference geometry
 	mdt = MeshDataTool.new()
 	var surface_tool = SurfaceTool.new()
@@ -53,7 +85,29 @@ func _process(_delta) -> void:
 			mesh.surface_set_uv(mdt.get_vertex_uv(vertex))
 			mesh.surface_add_vertex(vertex_grabbers[vertex].position)
 	mesh.surface_end()
+	
+	_calculate_emotion()
+	
 
+func _calculate_emotion() -> void:
+	sad_score.min_value = min_distance_score
+	sad_score.max_value = max_distance_score
+	
+	var sad_distance = 0.
+	var angry_distance = 0.
+	var amused_distance = 0.
+	
+	for vertex_idx in range (mdt.get_vertex_count()):
+		var curr_vertex = mdt.get_vertex(vertex_idx)
+		var sad_vertex = sad_mdt.get_vertex(vertex_idx)
+		var angry_vertex = angry_mdt.get_vertex(vertex_idx)
+		var amused_vertex = amused_mdt.get_vertex(vertex_idx)
+		
+		sad_distance += curr_vertex.distance_to(sad_vertex)
+		angry_distance += curr_vertex.distance_to(angry_vertex)
+		amused_distance += curr_vertex.distance_to(amused_vertex)
+		
+	sad_score.value = sad_distance
 
 func _export_current_mesh():
 	var export_mdt = MeshDataTool.new()
