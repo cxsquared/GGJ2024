f457114c8de67a52b65fd44243d4f052ee9d79ec
RedWheeler <ethannetsch@gmail.com>
2024-01-27 01:11:44 -0500

    Add drag range to VertexGrabber

--- vertex_grabber.gd
+++ vertex_grabber.gd
@@ -1,12 +1,18 @@
 extends Area3D
 
-var hovered = false
-var selected = false
+@export_range(0, 1, 0.1) var drag_range: float
+
+var initial_position: Vector3 = Vector3(0, 0, 0)
+var hovered: bool = false
+var selected: bool = false
+
+func _ready() -> void:
+	initial_position = position
 
 
-func _process(delta):
-	if selected:
-		# TODO: This doesn't work correctly. Can only move the grabbers in one direction.
+func _process(delta) -> void:
+	if selected and (position - initial_position).length() < drag_range:
+		# TODO: This doesn't work correctly
 		var mouse_position = get_viewport().get_mouse_position()
 		var camera = get_viewport().get_camera_3d()
 		var origin = camera.project_ray_origin(mouse_position)
