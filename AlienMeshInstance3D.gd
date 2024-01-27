fe8a66d0d29fd21e6213e3e5521efbd2314c2be7
RedWheeler <ethannetsch@gmail.com>
2024-01-27 00:28:31 -0500

    Implement grabber behavior that allows distortion of the mesh

--- AlienMeshInstance3D.gd
+++ AlienMeshInstance3D.gd
@@ -2,17 +2,20 @@ extends MeshInstance3D
 
 const NUMBER_OF_VERTICES: int = 3
 
+var vertex_grabbers: Dictionary = {}
+var mdt: MeshDataTool = null
+
 @onready var VertexGrabberScene = preload("res://vertex_grabber.tscn")
 
-func _ready():
-	var mdt = MeshDataTool.new()
-	
+func _ready() -> void:
+	# Init the MeshDataTool with the reference geometry
+	mdt = MeshDataTool.new()
 	var surface_tool = SurfaceTool.new()
-	surface_tool.create_from($MeshInstance3D.mesh, 0)
+	surface_tool.create_from($ReferenceMesh.mesh, 0)
 	var array_mesh = surface_tool.commit()
-
 	mdt.create_from_surface(array_mesh, 0)
 	
+	# Create the initial mesh. This should be the same as the reference geometry
 	mesh.surface_begin(NUMBER_OF_VERTICES)
 	for face_idx in range (mdt.get_face_count()):
 		for vertex_idx in range(NUMBER_OF_VERTICES):
@@ -27,3 +30,17 @@ func _ready():
 		var vertex_grabber = VertexGrabberScene.instantiate()
 		vertex_grabber.position = mdt.get_vertex(i)
 		add_child(vertex_grabber)
+		vertex_grabbers[i] = vertex_grabber
+
+
+func _process(_delta) -> void:
+	# Recreate the mesh using the grabbers' positions
+	mesh.clear_surfaces()
+	mesh.surface_begin(NUMBER_OF_VERTICES)
+	for face_idx in range (mdt.get_face_count()):
+		for vertex_idx in range(NUMBER_OF_VERTICES):
+			var vertex = mdt.get_face_vertex(face_idx, vertex_idx)
+			mesh.surface_set_normal(mdt.get_vertex_normal(vertex))
+			mesh.surface_set_uv(mdt.get_vertex_uv(vertex))
+			mesh.surface_add_vertex(vertex_grabbers[vertex].position)
+	mesh.surface_end()
--- vertex_grabber.gd
+++ vertex_grabber.gd
@@ -4,16 +4,24 @@ var hovered = false
 var selected = false
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if selected:
+		# TODO: This doesn't work correctly. Can only move the grabbers in one direction.
+		var mouse_position = get_viewport().get_mouse_position()
+		var camera = get_viewport().get_camera_3d()
+		var origin = camera.project_ray_origin(mouse_position)
+		position = Vector3(origin.x, origin.y, position.z)
 
 
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.is_pressed():
+			if hovered:
+				selected = true
+		elif event.is_released():
+			selected = false
+		
+		
 func _on_mouse_entered():
 	hovered = true
 
