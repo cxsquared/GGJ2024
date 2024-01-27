f6f0e5ec566a464752037deda0c60e38b8ac57dd
RedWheeler <ethannetsch@gmail.com>
2024-01-26 23:47:07 -0500

    Fix ImmediateMesh generation so that vertices are now instantiated properly

--- AlienMeshInstance3D.gd
+++ AlienMeshInstance3D.gd
@@ -1,23 +1,29 @@
 extends MeshInstance3D
 
+const NUMBER_OF_VERTICES: int = 3
+
 @onready var VertexGrabberScene = preload("res://vertex_grabber.tscn")
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	var mdt = MeshDataTool.new()
-	mdt.create_from_surface($ReferenceGeometry/Cube.mesh, 0)
-	# mesh.surface_set_material(0, $ReferenceGeometry/Cube.mesh.surface_get_material())
-	# Begin draw.
-	mesh.surface_begin(3)
-	print(mdt.get_vertex_count())
+	
+	var surface_tool = SurfaceTool.new()
+	surface_tool.create_from($MeshInstance3D.mesh, 0)
+	var array_mesh = surface_tool.commit()
+
+	mdt.create_from_surface(array_mesh, 0)
+	
+	mesh.surface_begin(NUMBER_OF_VERTICES)
+	for face_idx in range (mdt.get_face_count()):
+		for vertex_idx in range(NUMBER_OF_VERTICES):
+			var vertex = mdt.get_face_vertex(face_idx, vertex_idx)
+			mesh.surface_set_normal(mdt.get_vertex_normal(vertex))
+			mesh.surface_set_uv(mdt.get_vertex_uv(vertex))
+			mesh.surface_add_vertex(mdt.get_vertex(vertex))
+	mesh.surface_end()
+	
+	# Add the vertex grabbers
 	for i in range(mdt.get_vertex_count()):
 		var vertex_grabber = VertexGrabberScene.instantiate()
 		vertex_grabber.position = mdt.get_vertex(i)
 		add_child(vertex_grabber)
-		
-		mesh.surface_set_normal(mdt.get_vertex_normal(i))
-		mesh.surface_set_uv(mdt.get_vertex_uv(i))
-		mesh.surface_add_vertex(mdt.get_vertex(i))
-		
-	# End drawing.
-	mesh.surface_end()
