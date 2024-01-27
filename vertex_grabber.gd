c8871f1398df279a1be35f852e370b7ec86c44e3
RedWheeler <ethannetsch@gmail.com>
2024-01-27 02:14:10 -0500

    Assorted cleanup
    - Add indicator to VertexGrabber so that it is easier to detect when a vertex is selectable or not
    - Fix bug causing vertex grabbing to modify 3rd axis. Turns out it was just the y axis that shouldn't change, not z.
    - Fix bug causing VertexGrabbers to not be resectable if they were dragged to the maximum range.
    - Change VertexGrabbers collision shapes to cylinders. These are easier to cram next to eachother so there is less dead space between vertices.
    - Decrease mesh subdivisions. This was excessively expensive and actually made the mesh editing worse.

--- vertex_grabber.gd
+++ vertex_grabber.gd
@@ -3,34 +3,47 @@ extends Area3D
 @export_range(0, 1, 0.1) var drag_range: float
 
 var initial_position: Vector3 = Vector3(0, 0, 0)
+var mouse_pressed: bool = false
 var hovered: bool = false
 var selected: bool = false
 
 func _ready() -> void:
 	initial_position = position
+	print(initial_position)
 
 
 func _process(delta) -> void:
-	if selected and (position - initial_position).length() < drag_range:
-		# TODO: This doesn't work correctly
+	if selected:
 		var mouse_position = get_viewport().get_mouse_position()
 		var camera = get_viewport().get_camera_3d()
 		var origin = camera.project_ray_origin(mouse_position)
-		position = Vector3(origin.x, origin.y, origin.z)
+		var new_position = Vector3(origin.x, position.y, origin.z)
+		# If we are out of range, set the position to the max range in the same direction
+		if (new_position - initial_position).length() > drag_range:
+			position = initial_position + drag_range * (new_position - initial_position).normalized()
+		else:
+			position = new_position
 
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.is_pressed():
+			mouse_pressed = true
 			if hovered:
 				selected = true
 		elif event.is_released():
+			mouse_pressed = false
 			selected = false
+			$HoverIndicator.visible = hovered
 		
 		
 func _on_mouse_entered():
+	if !mouse_pressed:
+		$HoverIndicator.visible = true
 	hovered = true
 
 
 func _on_mouse_exited():
+	if !selected:
+		$HoverIndicator.visible = false
 	hovered = false
